a0ae804371eeced0350f88e130d4ea61be7632d8
EmersonMatthias <emersonkilon@gmail.com>
2022-08-21 13:52:02 -0300

    versão 1.1

--- projeto4.js
+++ projeto4.js
@@ -1,18 +1,30 @@
-let nCartas = Number(prompt("Com quantas cartas deseja jogar"));
+let nCartas;
 
-let finalizador = nCartas/2;
+let finalizador;
 
 let comparador;
 
-if ((nCartas%2) === 0 && nCartas > 3 && nCartas < 15 ){
-    alert(comparador = true);
-}
-else{
-    alert(comparador = false);
-}
+let firstcard;
+let secondcard;
+let a;
+let b;
+let c;
+let repetirjogo;
+let contadorjogadas = 0;
+let lockcard = false;
+let lockcardfirst;
+let lockcardfirst1;
 
-while(comparador === false){
+
+const cartas = ['bobrossparrot.gif','explodyparrot.gif','fiestaparrot.gif','metalparrot.gif','revertitparrot.gif','tripletsparrot.gif','unicornparrot.gif']
+
+let jogo = document.querySelector('.jogo');
+
+
+
+function selecao_cartas(){
     nCartas = Number(prompt("Com quantas cartas deseja jogar"));
+    finalizador = nCartas/2;
 
     if ((nCartas%2) === 0 && nCartas > 3 && nCartas < 15 ){
         alert(comparador = true);
@@ -20,101 +32,40 @@ while(comparador === false){
     else{
         alert(comparador = false);
     }
-    alert(comparador);
-}
 
+    while(comparador === false){
+        nCartas = Number(prompt("Com quantas cartas deseja jogar"));
 
-const cartas = ['bobrossparrot.gif','explodyparrot.gif','fiestaparrot.gif','metalparrot.gif','revertitparrot.gif','tripletsparrot.gif','unicornparrot.gif']
-
-let jogo = document.querySelector('.jogo');
-
-function comparado() { 
-    return Math.random() - 0.5; 
-}
-
-
-let cartas_sortidas = [];
-
-let cont = 0;
-
-let mcartas = nCartas/2;
-
-while( mcartas>cont){
-    cartas_sortidas.push(cartas[cont]);
-    cartas_sortidas.push(cartas[cont]);
-    cont++;
-    console.log(cartas_sortidas);
-}
-
-let firstcard;
-let secondcard;
-let a;
-let b;
-let c;
-
-function viracarta(cartaclicada){
-    cartaclicada.classList.add('vira');
-    cartaclicada.querySelector('.back').classList.remove('hidden');
-    cartaclicada.querySelector('.front').classList.add('hidden');
-
-    if(firstcard === undefined){
-        firstcard = cartaclicada;
-        a = firstcard.dataset.card;
-        console.log(a);
-    }
-    else if (secondcard === undefined){
-        secondcard = cartaclicada;
-        b = secondcard.dataset.card;
-        console.log(b);
-    }
-    console.log(firstcard);
-    console.log(secondcard);
-
-    
-    setTimeout(check,1000);
-    setTimeout(finalizajogo,1001);
-    
-    
-    
-  
-}
-
-
-
-function check(){
-    if (firstcard !== undefined && secondcard !== undefined){
-        c = a === b;
-        if(c !== true){
-            firstcard.classList.remove('vira');
-            firstcard.querySelector('.back').classList.add('hidden');
-            firstcard.querySelector('.front').classList.remove('hidden');
-
-            secondcard.classList.remove('vira');
-            secondcard.querySelector('.back').classList.add('hidden');
-            secondcard.querySelector('.front').classList.remove('hidden');
-            firstcard = undefined;
-            secondcard = undefined;
+        if ((nCartas%2) === 0 && nCartas > 3 && nCartas < 15 ){
+            alert(comparador = true);
         }
-        else if (c === true){
-            firstcard = undefined;
-            secondcard = undefined;
-            finalizador--;
-            console.log(finalizador);
+        else{
+            alert(comparador = false);
         }
-    }
-
-}
-
-function finalizajogo(){
-    if (finalizador === 0){
-        alert('você ganhou!')
+        alert(comparador);
     }
 }
 
+function posiciona_cartas(){
 
-function loadgame(){
+
+    let cartas_sortidas = [];
+
+    let cont = 0;
+
+    let mcartas = nCartas/2;
+
+    while( mcartas>cont){
+        cartas_sortidas.push(cartas[cont]);
+        cartas_sortidas.push(cartas[cont]);
+        cont++;
+        console.log(cartas_sortidas);
+    }
+
+    function comparado() { 
+        return Math.random() - 0.5; 
+    }
     
-
     
     cartas_sortidas = cartas_sortidas.sort(comparado);
     console.log(cartas_sortidas);
@@ -129,15 +80,112 @@ function loadgame(){
         <img class="front" src="/imagens/front.png">
     </div>`
     contador++
+    }
+
+    
+}
+
+function desativacarta(){
+    firstcard.innerHTML = `<img class="back" src="/imagens/${a}">`;
+
+}
+
+
+function viracarta(cartaclicada){
+    lockcardfirst = cartaclicada.innerHTML;
+    contadorjogadas++;
+    lockcardfirst1 = cartaclicada.querySelector('img').classList.contains('hidden');
+    if (lockcard === true) {return false};
+    if (lockcardfirst === `<img class="back" src="/imagens/${a}">`) {return false};
+    if (lockcardfirst1 === false){return false};
+    cartaclicada.classList.add('vira');
+    cartaclicada.querySelector('.back').classList.remove('hidden');
+    cartaclicada.querySelector('.front').classList.add('hidden');
+
+    if(firstcard === undefined){
+        firstcard = cartaclicada;
+        a = firstcard.dataset.card;
+        console.log(a);
+        desativacarta();
+        
+    }
+    else if (secondcard === undefined){
+        secondcard = cartaclicada;
+        b = secondcard.dataset.card;
+        console.log(b);
+        lockcard = true;
+    }
+    console.log(firstcard);
+    console.log(secondcard);
+
+    setTimeout(check,1000);
+
+}
+
+
+function check(){
+    if (firstcard !== undefined && secondcard !== undefined){
+        c = a === b;
+        if(c !== true){
+            firstcard.innerHTML = `<img class="back hidden" src="/imagens/${a}">
+            <img class="front" src="/imagens/front.png">`
+            firstcard.classList.remove('vira');
+            firstcard.querySelector('.back').classList.add('hidden');
+            firstcard.querySelector('.front').classList.remove('hidden');
+
+            secondcard.classList.remove('vira');
+            secondcard.querySelector('.back').classList.add('hidden');
+            secondcard.querySelector('.front').classList.remove('hidden');
+            firstcard = undefined;
+            secondcard = undefined;
+            lockcard = false;
+            
+        }
+        else if (c === true){
+            firstcard.innerHTML = `<img class="back" src="/imagens/${a}">`
+            secondcard.innerHTML = `<img class="back" src="/imagens/${b}">`
+            firstcard = undefined;
+            secondcard = undefined;
+            finalizador--;
+            console.log(finalizador);
+            lockcard = false;
+        }
+    
+        setTimeout(finalizajogo,1001);
+        
+        
+    }
+}
+
+function finalizajogo(){
+    if (finalizador === 0){
+        alert(`Você ganhou o jogo em ${contadorjogadas} jogadas`);
+        repetirjogo = prompt("Você deseja continuar (sim ou não)");
+
+        if(repetirjogo === "sim"){
+            jogo.innerHTML = "";
+            contadorjogadas = 0;
+            finalizador = 0;
+            loadgame();
+        }
+        else{
+            alert('O JOGO ACABOU')
+        }
+    }
+}
+
+
+function loadgame(){
+    /*SELEÇÃO DE CARTAS*/ 
+    selecao_cartas();
+
+    posiciona_cartas();
+    
 }
     
-
-
-    }
-    
-    
-
 loadgame();
+
+
 
 
 
